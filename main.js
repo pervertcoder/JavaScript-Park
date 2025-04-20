e696b1c0c21ebb578cd7709dec1cb0abf49aba72
pervertcoder <lance60702@gmail.com>
2025-04-21 00:47:11 +0800

    4/20
    2-9

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log(23);
+let firstName = "Willy";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
