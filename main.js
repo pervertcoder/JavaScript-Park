41ceb188b6f90324d5d858b4e8d87d5b686980e6
pervertcoder <lance60702@gmail.com>
2025-04-22 01:07:22 +0800

    4/21  2-15
    2-15 code challenge 1

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let js = 'amazing';
+/*let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 console.log("Jonas");
 console.log(23);
@@ -6,4 +6,76 @@ let firstName = "Willy";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+let country = "Taiwan";
+let continent = "island";
+let population = "23,000,000";
+console.log(country);
+console.log(continent);
+console.log(population);
+// Variable name convention
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
 
+
+javascriptIsFun = "Yes!";
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1998;
+console.log(year);
+console.log(typeof null);
+
+
+let age = 30;
+age = 35;
+console.log(age);
+
+const birthYear = 1998;
+console.log(birthYear);
+
+var job = "programmer";
+console.log(job);
+*/
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2 *
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5; //15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+
+console.log(ageJonas < ageSarah); // >, <, <=, >=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018);
+
+console.log(25 - 10 - 5);
+
+let z, y;
+z = y = 25 - 10 - 5;
+console.log(z, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
