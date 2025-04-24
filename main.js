07a2fcd93b603f4256bc1a911f0b4d41c73f7c27
pervertcoder <lance60702@gmail.com>
2025-04-24 15:09:27 +0800

    4/24 2 end
    complete the second part

--- main.js
+++ main.js
@@ -132,7 +132,7 @@ if (age >= 18) {
     console.log("Sarah can start driving license");
 } else {
     const yearsLeft = 18 - age;
-    console.log(`Sarah is too young. Wait another 
+    console.log(`Sarah is too young. Wait another
 ${yearsLeft} years`);
 }
 
@@ -147,6 +147,7 @@ if (birthDay <= 2000) {
 console.log(century);*/
 
 //conversion section
+/*
 const inputYear = "1991";
 console.log(Number(inputYear), inputYear);
 console.log(Number(inputYear) + 18);
@@ -167,12 +168,127 @@ let n = "1" + 1; // "11"
 n = n - 1;
 console.log(n);
 console.log(typeof n);
-/*
-+ can work as string with string or string with number, so it turns out to be string as well
-however, minus, multiplication and division, can not work with string, so in the case of string with string and string with number, it will turn out to be number because of coercion
-*/
+
+// + can work as string with string or string with number, so it turns out to be string as well
+// however, minus, multiplication and division, can not work with string, so in the case of string with string and string with number, it will turn out to be number because of coercion
+
 
 // tricky case
 let m = "10" - "4" - 3 - "2" + "5";
 console.log(m);
 console.log(typeof m);
+*/
+
+/*
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 100;
+if (money) {
+    console.log("Don't spend it all");
+} else {
+    console.log("You should get a job!");
+}
+
+let height = 20;
+if (height) {
+    console.log("height is well defined");
+} else {
+    console.log("height is not defined");
+}
+*/
+/*
+const age = "18";
+if (age === 18) console.log("You just became an adult(strict)");
+
+if (age == 18) console.log("You just became an adult(loose)");
+
+const favorite = Number(prompt("What's your favorite number?"));
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 23) {
+    console.log("Cool! 23 is an amazing number!");
+} else if (favorite === 7) {
+    console.log("7 is also a cool number");
+} else {
+    console.log("Number is not 23 or7");
+}
+
+if (favorite !== 23) console.log("Why not 23?");
+*/
+/*
+const hasDrivrsLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDrivrsLicense && hasGoodVision);
+console.log(hasDrivrsLicense || hasGoodVision);
+console.log(!hasDrivrsLicense);
+
+
+if (hasDrivrsLicense && hasGoodVision) {
+    console.log("Sarah is able to drive");
+} else {
+    console.log("Someone else should drive");
+}
+
+const isTired = false; // C
+console.log(hasDrivrsLicense && hasGoodVision && !isTired);
+
+if (hasDrivrsLicense && hasGoodVision && !isTired) {
+    console.log("Sarah is able to drive");
+} else {
+    console.log("Someone else should drive");
+}
+*/
+/*
+const day = "wednesday";
+
+switch (day) {
+    case "monday":
+        console.log("Plan couse structure");
+        console.log("Go to coding meetup");
+        break;
+    case "tuesday":
+        console.log("Prepare theory videos");
+        break;
+    case "wednesday":
+    case "thursday":
+        console.log("Write code examples");
+        break;
+    case "friday":
+        console.log("Record videos");
+        break;
+    case "saturday":
+    case "sunday":
+        console.log("Enjoy the weekend");
+    default:
+        console.log("Not a valid day");
+}
+*/
+/*
+const day = "saturday";
+
+if (day === "monday") {
+    console.log("Plan couse structure");
+    console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+    console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+    console.log("Write code examples");
+} else if (day === "friday") {
+    console.log("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+    console.log("Enjoy the weekend");
+} else {
+    console.log("Not a valid day");
+}
+*/
+const age = 18;
+const drink = age >= 18 ? "wine" : "water";
+console.log(drink);
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
